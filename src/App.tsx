c5bacb88471f244755d88843201538b2f9b10132
Lealzzera <matheus.mleall@gmail.com>
2022-10-26 16:30:05 -0300

    removing some stuffs

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,25 @@
-import React from 'react';
-import PostForm from './components/PostForm';
-import { Post } from './types/Post';
-import PostItem from './components/PostItem';
-import { api } from './api/api';
-import foto from './assets/familyMoney.png';
-import AppBar from './components/AppBar'
+import React from 'react'
 
+type reducerState = {
+  count: number;
+}
+
+type reducerAction = {
+  type: string;
+
+}
 
 const App = () => {
+  const initialState = { count: 0 };
 
-  let image = './assets/familyMoney.png'
-  const [post, setPost] = React.useState<Post[]>();
-  const [loading, setLoading] = React.useState<boolean>(false);
-
-
-  React.useEffect(() => {
-    loadPosts();
-  }, []);
-
-  const loadPosts = async () => {
-    setLoading(true);
-    let json = await api.getAllPosts()
-    setLoading(false);
-    setPost(json);
-  };
-
-  const addNewPost = async (title: string, body: string) => {
-    api.postNewPost(title, body, 1);
+  const reducer = (state: reducerState, action: reducerAction) => {
+    return state
   }
 
-  return (
-    <div className='bg-blue-200'>
-      <AppBar />
-      {loading && <div>Carregando...</div>}
-      <div>
-      </div>
-      {!loading &&
-        <>
-          <div>Total posts: {post?.length}</div>
-          <div>
-            <PostForm addPost={addNewPost} />
-            {post?.map((item) => (
-              <PostItem key={item.id} data={item} />
-            ))}
-          </div>
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
-        </>}
-      <img src={foto} alt="" />
-    </div>
+  return (
+    <div>App</div>
   )
 }
 
